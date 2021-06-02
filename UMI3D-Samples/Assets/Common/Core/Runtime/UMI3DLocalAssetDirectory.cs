ddebd20d4844c502ab733fb1721df3ed2d327f69
Theo-Desbonnets <theo.desbonnets@inetum.world>
2021-06-02 17:29:04 +0200

    wip

---
+++ UMI3D-Samples/Assets/Common/Core/Runtime/UMI3DLocalAssetDirectory.cs
@@ -0,0 +1,66 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace umi3d.common
+{
+    [System.Serializable]
+    public class UMI3DLocalAssetDirectory : IByte
+    {
+        public string name = "new variant";
+        public string path;
+        [SerializeField]
+        public AssetMetricDto metrics = new AssetMetricDto();
+        [ConstStringEnum(typeof(UMI3DAssetFormat))]
+        public List<string> formats = new List<string>();
+
+        public UMI3DLocalAssetDirectory()
+        {
+        }
+
+        public UMI3DLocalAssetDirectory(UMI3DLocalAssetDirectory other)
+        {
+            this.name = other.name;
+            this.path = other.path;
+            this.metrics = other.metrics;
+            this.formats = other.formats;
+        }
+
+        (int, Func<byte[], int, int>) IByte.ToByteArray(params object[] parameters)
+        {
+            int size =
+                UMI3DNetworkingHelper.GetSize(name)
+                + UMI3DNetworkingHelper.GetSize(path)
+                + UMI3DNetworkingHelper.GetSize(metrics.resolution)
+                + UMI3DNetworkingHelper.GetSize(metrics.size)
+                + UMI3DNetworkingHelper.GetSizeArray(formats);
+            Func<byte[], int, int> func = (b, i) =>
+            {
+                i += UMI3DNetworkingHelper.Write(name, b, i);
+                i += UMI3DNetworkingHelper.Write(path, b, i);
+                i += UMI3DNetworkingHelper.Write(metrics.resolution, b, i);
+                i += UMI3DNetworkingHelper.Write(metrics.size, b, i);
+                i += UMI3DNetworkingHelper.WriteArray(formats, b, i);
+                return size;
+            };
+            return (size, func);
+        }
+        //public List<string> dependencies = new List<string>();
+    }
+}
---
+++ UMI3D-Samples/Assets/EnvironmentDevelopmentKit/Core/Runtime/Scene synchronisation/SetEntityProperty.cs
@@ -0,0 +1,82 @@
+﻿/*
+Copyright 2019 - 2021 Inetum
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using umi3d.common;
+
+namespace umi3d.edk
+{
+    /// <summary>
+    /// An operation to inform users the value of an entity's property changed.
+    /// </summary>
+    public class SetEntityProperty : Operation
+    {
+        /// <summary>
+        /// The unique identifier of the entity
+        /// </summary>
+        public ulong entityId;
+
+        /// <summary>
+        /// The name of the modified property
+        /// </summary>
+        public uint property;
+
+        /// <summary>
+        /// The new value for the property
+        /// </summary>
+        public object value;
+
+        public override (int, Func<byte[], int, int>) ToBytes(UMI3DUser user)
+        {
+            int size = sizeof(uint) + sizeof(ulong) + sizeof(uint) + UMI3DNetworkingHelper.GetSize(value);
+            Func<byte[], int, int> func = (b, i) => {
+                i += UMI3DNetworkingHelper.Write(UMI3DOperationKeys.SetEntityProperty, b, i);
+                i += UMI3DNetworkingHelper.Write(entityId, b, i);
+                i += UMI3DNetworkingHelper.Write(property, b, i);
+                i += UMI3DNetworkingHelper.Write(value, b, i);
+                return size;
+            };
+            return (size, func);
+        }
+
+        ///<inheritdoc/>
+        public override AbstractOperationDto ToOperationDto(UMI3DUser user)
+        {
+            var setEntity = new SetEntityPropertyDto();
+            setEntity.property = property;
+            setEntity.entityId = entityId;
+            setEntity.value = value;
+            return setEntity;
+        }
+
+        public static SetEntityProperty operator +(SetEntityProperty a, IEnumerable<UMI3DUser> b)
+        {
+            a.users = new HashSet<UMI3DUser>(a.users.Concat(b));
+            return a;
+        }
+        public static SetEntityProperty operator -(SetEntityProperty a, IEnumerable<UMI3DUser> b)
+        {
+            foreach (var u in b)
+            {
+                if (a.users.Contains(u)) a.users.Remove(u);
+            }
+            return a;
+        }
+
+    }
+}
