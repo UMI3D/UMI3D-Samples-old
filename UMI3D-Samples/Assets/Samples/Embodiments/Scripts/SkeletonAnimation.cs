d3ab239623c5fe25bccf5783e66e7c5b9dad7863
Nicolas Pacqueriaud <nicolas.pacqueriaud@gfi.fr>
2021-06-14 16:10:37 +0200

    Updating SDK to 2.3.b.210610 & Adding Embodiment Test Scene

---
+++ UMI3D-Samples/Assets/Samples/Embodiments/Scripts/SkeletonAnimation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using umi3d.common.userCapture;
+using umi3d.common;
+using umi3d.edk;
+using umi3d.edk.userCapture;
+using System.Linq;
+
+public class SkeletonAnimation : MonoBehaviour
+{
+    public GameObject SkeletonPrefab;
+
+    Dictionary<string, GameObject> skeletons = new Dictionary<string, GameObject>();
+    Dictionary<string, Animator> animators = new Dictionary<string, Animator>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UMI3DEmbodimentManager.Instance.NewEmbodiment.AddListener(SkeletonInstantiation);
+        UMI3DEmbodimentManager.Instance.UpdateEvent.AddListener(UpdateBone);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SkeletonInstantiation(UMI3DAvatarNode node)
+    {
+        Vector3 userSize = UMI3DEmbodimentManager.Instance.embodimentSize[node.userId];
+        GameObject skeleton = Instantiate(SkeletonPrefab, node.transform);
+        skeletons.Add(node.userId, skeleton);
+        skeleton.transform.localScale = userSize;
+        animators.Add(node.userId, skeleton.GetComponentInChildren<Animator>());
+    }
+
+    void UpdateBone(UMI3DUserEmbodimentBone bone)
+    {
+        Animator userAnimator = animators[bone.userId];
+        Transform transform = userAnimator.GetBoneTransform(BoneTypeConverter.Convert(bone.boneType).GetValueOrDefault());
+        transform.localRotation = bone.spatialPosition.localRotation;
+    }
+}
