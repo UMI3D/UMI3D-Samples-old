146ce9eacd6686e8b67de7ebd3f9574dc7525d9b
Nicolas Pacqueriaud <nicolas.pacqueriaud@gfi.fr>
2021-11-16 14:48:59 +0100

    Adding Hand Poses stand, Bindings stand and Interpolation stand

---
+++ UMI3D-Samples/Assets/Samples/TestRoom/Scripts/InterpolationManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InterpolationManager : MonoBehaviour
+{
+    public bool updateTransform = false;
+    public bool isInterpolating = false;
+
+    public UnityEvent UpdateEvent;
+    public UnityEvent InterpolationEvent;
+
+    public void UpdateStatus()
+    {
+        updateTransform = !updateTransform;
+        UpdateEvent.Invoke();
+    }
+
+    public void InterpolationStatus()
+    {
+        isInterpolating = !isInterpolating;
+        InterpolationEvent.Invoke();
+    }
+}
