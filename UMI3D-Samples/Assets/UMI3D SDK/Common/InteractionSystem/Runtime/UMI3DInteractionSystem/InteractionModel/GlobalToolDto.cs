79ec27da8a05b83b2dc881ee6b2fb7bd4d1aa5fe
Romain Tirbisch <romain.tirbisch@inetum.com>
2022-03-02 14:12:23 +0100

    Update to sdk 2.5.b.220301

--- UMI3D-Samples/Assets/UMI3D SDK/Common/InteractionSystem/Runtime/UMI3DInteractionSystem/InteractionModel/GlobalToolDto.cs
+++ UMI3D-Samples/Assets/UMI3D SDK/Common/InteractionSystem/Runtime/UMI3DInteractionSystem/InteractionModel/GlobalToolDto.cs
@@ -1,10 +1,4 @@
-# Introduction 
-A collection of utils class use through multiple unity project.
-
-# Licence
-
-Some of those file are based on post on forum as [Unity](https://answers.unity.com/index.html), [Stack Overflow](https://stackoverflow.com/) and others
-
+﻿/*
 Copyright 2019 - 2021 Inetum
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,6 +12,19 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
+*/
 
-# Version
-1.2.220301
+namespace umi3d.common.interaction
+{
+    public class GlobalToolDto : AbstractToolDto, IEntity
+    {
+        /// <summary>
+        /// Toolbox in which the global tool is (if any).
+        /// </summary>
+        /// <see cref="isInsideToolbox"/>
+        public ulong toolboxId;
+        public bool isInsideToolbox;
+
+        public GlobalToolDto() : base() { }
+    }
+}
