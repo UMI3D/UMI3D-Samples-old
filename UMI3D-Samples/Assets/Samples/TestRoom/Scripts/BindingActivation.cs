0f9461035616e53076993e708a492b19fda43eb4
Quentin Tran <quentin.tran@inetum.com>
2022-09-08 12:11:53 +0200

    Fix binding stand

--- UMI3D-Samples/Assets/Samples/TestRoom/Scripts/BindingActivation.cs
+++ UMI3D-Samples/Assets/Samples/TestRoom/Scripts/BindingActivation.cs
@@ -21,38 +21,83 @@ using umi3d.edk.userCapture;
 using umi3d.common.userCapture;
 using UnityEngine;
 
+[RequireComponent(typeof(UMI3DNode))]
 public class BindingActivation : MonoBehaviour
 {
+    #region Fields
+
+    [Tooltip("Distance to grab the object with hands")]
     public float HandDistActivation;
 
+    /// <summary>
+    /// Orignal local position of <see cref="node"/>.
+    /// </summary>
     Vector3 tempLocalPos;
+
+    /// <summary>
+    /// Original local rotation of <see cref="node"/>.
+    /// </summary>
     Quaternion tempLocalRot;
+
+    /// <summary>
+    /// Current binding when <see cref="node"/> is grabbed.
+    /// </summary>
     UMI3DBinding tempBinding;
+
+    /// <summary>
+    /// Id of the current <see cref="UMI3DUser"/> who is grabbing <see cref="node"/>.
+    /// </summary>
     ulong tempUserID;
 
+    /// <summary>
+    /// Transform of the bone which is currently grabbing <see cref="node"/>.
+    /// </summary>
     Transform bindingAnchor;
+
+    /// <summary>
+    /// Local position offset between <see cref="node"/> and <see cref="bindingAnchor"/>.
+    /// </summary>
     Vector3 localPosOffset;
+
+    /// <summary>
+    /// Local rotation offset between <see cref="node"/> and <see cref="bindingAnchor"/>.
+    /// </summary>
     Quaternion localRotOffset;
 
+    /// <summary>
+    /// Is the object grabbed ?
+    /// </summary>
     bool activation = false;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Reference to the node grabbed.
+    /// </summary>
+    UMI3DNode node;
+
+    #endregion
+
     void Start()
     {
         tempLocalPos = transform.localPosition;
         tempLocalRot = transform.localRotation;
+
+        node = GetComponent<UMI3DNode>();
+        Debug.Assert(node != null);
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (activation)
         {
-            GetComponent<UMI3DNode>().objectPosition.SetValue(transform.parent.InverseTransformPoint(bindingAnchor.TransformPoint(localPosOffset)));
-            GetComponent<UMI3DNode>().objectRotation.SetValue(Quaternion.Inverse(transform.parent.rotation) * bindingAnchor.rotation * localRotOffset);
+            node.objectPosition.SetValue(transform.parent.InverseTransformPoint(bindingAnchor.TransformPoint(localPosOffset)));
+            node.objectRotation.SetValue(Quaternion.Inverse(transform.parent.rotation) * bindingAnchor.rotation * localRotOffset);
         }
     }
 
+    /// <summary>
+    /// Grabs the object or releases it if it was already hold by a user.
+    /// </summary>
+    /// <param name="content"></param>
     public void UpdateBindingActivation(umi3d.edk.interaction.AbstractInteraction.InteractionEventContent content)
     {
         UMI3DTrackedUser user = content.user as UMI3DTrackedUser;
@@ -60,14 +105,13 @@ public class BindingActivation : MonoBehaviour
 
         if (!activation)
         {
-            if (!bonetype.Equals(BoneType.Head) && Vector3.Distance(transform.position, user.Avatar.skeletonAnimator.GetBoneTransform(bonetype.ConvertToBoneType().GetValueOrDefault()).transform.position) > HandDistActivation)
+            if (!bonetype.Equals(BoneType.Viewpoint) && Vector3.Distance(transform.position, user.Avatar.skeletonAnimator.GetBoneTransform(bonetype.ConvertToBoneType().GetValueOrDefault()).transform.position) > HandDistActivation)
                 return;
 
             bindingAnchor = user.Avatar.skeletonAnimator.GetBoneTransform(bonetype.ConvertToBoneType().GetValueOrDefault()).transform;
-
             activation = true;
 
-            localPosOffset = bindingAnchor.InverseTransformPoint(transform.position);
+            localPosOffset = bonetype.Equals(BoneType.Viewpoint) ? Vector3.forward * 1.5f : bindingAnchor.InverseTransformPoint(transform.position);
             localRotOffset = Quaternion.Inverse(bindingAnchor.rotation) * transform.rotation;
 
             tempUserID = user.Id();
@@ -109,6 +153,4 @@ public class BindingActivation : MonoBehaviour
             UMI3DServer.Dispatch(transaction);
         }
     }
-
-
 }
